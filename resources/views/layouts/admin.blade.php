e79e4e0e1fda57159eb29d170f955ba1cb2cdc97
magallon <christian1350@gmail.com>
2016-07-04 12:16:36 -0500

    agragando icono cerrar sesion

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -33,17 +33,17 @@
                 </button>
             </div>
             <ul class="nav navbar-top-links navbar-right">
-                 <li class="dropdown">
+                {{--  <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Ajustes</a>
+                    </a> --}}
+                    
+                        {{-- <li><a href="#"><i class="fa fa-gear fa-fw"></i> Ajustes</a>
                         </li>
-                        <li class="divider"></li>
+                        <li class="divider"></li> --}}
                         <li><a href="/logout"><i class="fa fa-sign-out fa-fw"></i> Cerrar sesión</a>
                         </li>
-                    </ul>
+                    
                 </li>
             </ul>
             <div class="navbar-default sidebar" role="navigation">
